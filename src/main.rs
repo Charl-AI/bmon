5759e650ac616ed411876e8ba36a4358680a25c5
C Jones <cj1917@ic.ac.uk>
2023-05-06 17:47:33 +0100

    add verbose mode (throttling not working)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use nvml_wrapper::{Device, Nvml};
 
 use nvml_wrapper::enum_wrappers::device::TemperatureSensor;
-use tabled::{settings::object::Rows, settings::Disable, Table, Tabled};
+use tabled::{
+    settings::locator::ByColumnName, settings::object::Rows, settings::Disable, Table, Tabled,
+};
 
 #[derive(Tabled)]
+#[tabled(rename_all = "UPPERCASE")]
 struct GPUStats {
     idx: u32,
     name: String,
     temp: String,
     power: String,
-    utilization_rates: String,
+    utilizations: String,
     memory: String,
+
+    // these are not displayed unless verbose is true
+    capability: String,
+    cores: u32,
+    throttling: String,
+    fan: String,
+    display: String,
+    processes: String,
 }
 
 impl GPUStats {
@@ -31,7 +42,7 @@ impl GPUStats {
 
         let gpu_utilization = device.utilization_rates().unwrap().gpu;
         let memory_utilization = device.utilization_rates().unwrap().memory;
-        let utilization_rates = format!("GPU {}% MEM {}%", gpu_utilization, memory_utilization);
+        let utilizations = format!("GPU {}% MEM {}%", gpu_utilization, memory_utilization);
 
         let memory_used = device.memory_info().unwrap().used;
         let memory_total = device.memory_info().unwrap().total;
@@ -40,30 +51,121 @@ impl GPUStats {
             memory_used as f32 / 1024.0 / 1024.0 / 1024.0,
             memory_total as f32 / 1024.0 / 1024.0 / 1024.0
         );
+
+        let compute_cap = device.cuda_compute_capability().unwrap();
+        let capability = format!("{}.{}", compute_cap.major, compute_cap.minor);
+        let cores = device.num_cores().unwrap();
+
+        // let throttle_reasons = device.current_throttle_reasons().unwrap();
+        // let throttling = if throttle_reasons.is_empty() {
+        //     "None".to_string()
+        // } else {
+        //     throttle_reasons
+        //         .iter()
+        //         .map(|reason| format!("{:?}", reason))
+        //         .collect::<Vec<String>>()
+        //         .join(", ")
+        // };
+        let throttling = "N/A".to_string();
+
+        let n_fans = device.num_fans().unwrap();
+        // fans reports average speed of all fans
+        let mut sum_fans = 0;
+        for i in 0..n_fans {
+            sum_fans += device.fan_speed(i).unwrap();
+        }
+        let fan = format!("{}%", sum_fans / n_fans);
+
+        let display_connected = device.is_display_connected().unwrap();
+        let display_active = device.is_display_active().unwrap();
+        let display = if display_active {
+            "Active".to_string()
+        } else if display_connected {
+            "Connected".to_string()
+        } else {
+            "None".to_string()
+        };
+
+        let compute_processes = device.running_compute_processes().unwrap();
+        let processes = compute_processes
+            .iter()
+            .map(|process| process.pid.clone().to_string())
+            .collect::<Vec<String>>()
+            .join(", ");
+
         Self {
             idx,
             name,
             temp,
             power,
-            utilization_rates,
+            utilizations,
             memory,
+
+            capability,
+            cores,
+            throttling,
+            fan,
+            display,
+            processes,
         }
     }
 }
 
-fn main() {
-    let nvml = Nvml::init().unwrap();
-    let device_count = nvml.device_count().unwrap();
-    println!("Device count: {}", device_count);
+struct Machine {
+    cuda_version: String,
+    driver_version: String,
+    gpus: Vec<GPUStats>,
+}
 
-    let mut gpus: Vec<GPUStats> = vec![];
-    for i in 0..device_count {
-        let device = nvml.device_by_index(i).unwrap();
-        let gpu = GPUStats::from_nvml_device(device);
-        gpus.push(gpu);
+impl Machine {
+    fn new() -> Self {
+        let nvml = Nvml::init().unwrap();
+
+        // NB: cuda version begins as an int e.g. 12000
+        // this is converted to a float e.g. 12.0
+        let cuda_version = nvml.sys_cuda_driver_version().unwrap();
+        let cuda_version = cuda_version as f32 / 1000.0;
+        let cuda_version = format!("{:.1}", cuda_version);
+
+        let driver_version = nvml.sys_driver_version().unwrap();
+
+        let mut gpus: Vec<GPUStats> = vec![];
+        let num_gpus = nvml.device_count().unwrap();
+        for i in 0..num_gpus {
+            let device = nvml.device_by_index(i).unwrap();
+            let gpu = GPUStats::from_nvml_device(device);
+            gpus.push(gpu);
+        }
+
+        Self {
+            cuda_version,
+            driver_version,
+            gpus,
+        }
     }
 
-    let mut table = Table::new(gpus);
-    table.with(Disable::row(Rows::first()));
-    println!("{}", table);
+    fn display_gpu_stats(&self, verbose: bool) {
+        print!(
+            "CUDA Version {} | Driver Version {} \n",
+            self.cuda_version, self.driver_version
+        );
+        let mut table = Table::new(&self.gpus);
+        if !verbose {
+            table
+                .with(Disable::column(ByColumnName::new("CAPABILITY")))
+                .with(Disable::column(ByColumnName::new("CORES")))
+                .with(Disable::column(ByColumnName::new("THROTTLING")))
+                .with(Disable::column(ByColumnName::new("FAN")))
+                .with(Disable::column(ByColumnName::new("DISPLAY")))
+                .with(Disable::column(ByColumnName::new("PROCESSES")))
+                .with(Disable::row(Rows::first()));
+        }
+        println!("{}", table);
+    }
+}
+
+fn main() {
+    let machine = Machine::new();
+    let verbose = false;
+    machine.display_gpu_stats(verbose);
 }
